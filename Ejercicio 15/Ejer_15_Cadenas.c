0201d906cd96f758b1a6b149fccfd346f02ec704
Jos3GS <gutierrez.sossa.j@gmail.com>
2021-02-10 00:09:05 -0500

    Se sube ultimo herciccio realiza del dia

---
+++ Ejercicio 15/Ejer_15_Cadenas.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    char name[40];
+    printf("Como te llamas?\n");
+    if(fgets(name,40,stdin)){
+        name[strcspn(name, "\n")] =0;
+        printf("Hola %s!\n", name);
+    }
+}
