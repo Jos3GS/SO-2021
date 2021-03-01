e885671596afe02c9ea9db2ff488dbcc0c7811a1
Jos3GS <gutierrez.sossa.j@gmail.com>
2021-03-01 12:19:44 -0500

    se hace la modificacion al programa

--- Ejercicio 42/main.c
+++ Ejercicio 42/main.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
 #include "api.h"
 
 int main(int argc, char* argv[]){
-    printf("uno: %d\n",uno());
-    printf("dos: %d\n",dos());
-    printf("tres: %d\n",tres());
+    
+    int (*func_ptr)() = NULL;
+    void* handle = dlopen("./libstatic.so", RTLD_LAZY);
 
+    if(!handle){
+        fprintf(stderr, "%s\n", dlerror());
+        exit(1);
+    }
+
+    func_ptr = dlsym(handle, "uno");
+    if(!func_ptr){
+        fprintf(stderr, "%s\n", dlerror());
+        exit(1);
+    }
+    printf("uno(): %d\n", func_ptr());
+
+    func_ptr = dlsym(handle, "dos");
+    if(!func_ptr){
+        fprintf(stderr, "%s\n", dlerror());
+        exit(1);
+    }
+    printf("dos(): %d\n",func_ptr());
+
+    func_ptr = dlsym(handle, "tres");
+    if(!func_ptr){
+        fprintf(stderr, "%s\n", dlerror());
+        exit(1);
+    }
+    printf("tres(): %d\n",func_ptr());
     return 0;
 }
