019c61c4fb0318b0d785607288f6d77ab80ca42f
Jos3GS <gutierrez.sossa.j@gmail.com>
2021-03-09 23:49:06 -0500

    se realza ejercicio 64

---
+++ Ejercicio 64/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    char arr[14];
+
+    arr[0] = 'C';
+    arr[0] = 'o';
+    arr[0] = 'n';
+    arr[0] = 't';
+    arr[0] = 'r';
+    arr[0] = 'o';
+    arr[0] = 'l';
+    arr[0] = 'a';
+    arr[0] = 'd';
+    arr[0] = 'o';
+    arr[0] = 'r';
+    arr[0] = 'e';
+    arr[0] = 's';
+    arr[0] = 0;
+
+    printf("arr:  %s",arr);
+    return 0;
+}
